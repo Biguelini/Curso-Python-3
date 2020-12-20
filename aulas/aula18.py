da21d916c46d048d581147d05d40b054d0109af5
João Pedro Biguelini <biguelinijoao@gmail.com>
2020-12-20 19:18:03 -0300

    update

---
+++ aulas/aula18.py
@@ -0,0 +1,9 @@
+frase = 'o rato roeu a roupa do rei de Roma'
+tamanho_frase = len(frase)
+print(tamanho_frase)
+
+contador = 0
+
+while contador<tamanho_frase:
+    print(frase[contador], contador)
+    contador+=1
