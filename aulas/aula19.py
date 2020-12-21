92370b424fb0baa157c5705b476e023a35107626
João Pedro Biguelini <biguelinijoao@gmail.com>
2020-12-21 17:12:23 -0300

    update

---
+++ aulas/aula19.py
@@ -0,0 +1,14 @@
+frase = 'Esta é minha frase!'
+for letra in frase:
+    print(letra)
+print('################')
+# range(start, stop, step)
+#range(start=0, stop=10, step=1)
+for n in range(10):
+    print(n)
+print('################')
+for n in range(20, 28, 2):
+    print(n)
+print('################')
+for n in range(20, 10, -1):
+    print(n)
