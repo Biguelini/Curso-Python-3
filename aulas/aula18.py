0d0617cc4ba14e550cf3335c36b2c900ae62be1c
João Pedro Biguelini <biguelinijoao@gmail.com>
2020-12-21 17:02:38 -0300

    update

--- aulas/aula18.py
+++ aulas/aula18.py
@@ -3,7 +3,14 @@ tamanho_frase = len(frase)
 print(tamanho_frase)
 
 contador = 0
+nova_string = ''
 
 while contador<tamanho_frase:
-    print(frase[contador], contador)
+    # print(frase[contador], contador)
+    letra = frase[contador]
+    if letra == 'r':
+        nova_string += 'R'
+    else:
+        nova_string += letra
     contador+=1
+    print(nova_string)
