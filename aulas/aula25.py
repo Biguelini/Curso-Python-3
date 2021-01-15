062809800c208b1cc2dd2e26c0f9c6b319c36215
João Pedro Biguelini <biguelinijoao@gmail.com>
2021-01-15 20:55:09 -0300

    update

--- aulas/aula25.py
+++ aulas/aula25.py
@@ -1,4 +1,10 @@
-logged_user = True
+#operador ternário
 
-if logged_user:
-    print('Logado')
+logged_user = True
+msg = 'Logado' if (logged_user) else 'Precisa logar'
+
+idade = 25
+entrada = 'Liberado' if (idade >= 18) else 'Negado'
+
+print(entrada)
+print(msg)
