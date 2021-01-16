20ebcb9fea2209f918ac293e08a56efc76064303
João Pedro Biguelini <biguelinijoao@gmail.com>
2021-01-15 22:04:13 -0300

    update

---
+++ aulas/desafio3.py
@@ -0,0 +1,10 @@
+c1 = 0
+c2 = 10
+while(c1 <= 8 and c2 >=2 ):
+    print(c1,c2)
+    c1+=1
+    c2-=1
+
+# solução do instrutor
+# for cont1, cont2 in enumerate(range(10,1,-1)):
+#     print(cont1, cont2)
