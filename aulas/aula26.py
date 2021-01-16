543f64804b5d5b4613cfb979e23292373b582cb5
João Pedro Biguelini <biguelinijoao@gmail.com>
2021-01-15 21:56:54 -0300

    update

---
+++ aulas/aula26.py
@@ -0,0 +1,13 @@
+# nome = input('Qual seu nome? ')
+# print(nome or 'Você não digitou nada')
+
+a=0
+b=None
+c=False
+d={}
+e=''
+f=[]
+g=21
+h='oi'
+variavel = a or b or c or d or f or g or h
+print(variavel)
