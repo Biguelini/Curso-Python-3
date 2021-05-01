3e79ba00891b90608652aac2132ec10395904e76
João Pedro Biguelini <biguelinijoao@gmail.com>
2021-05-01 11:44:49 -0300

    update

---
+++ aulas/aula29.py
@@ -0,0 +1,12 @@
+lista = [
+    ['P1', 13],
+    ['P2', 24],
+    ['P3', 42],
+    ['P4', 113],
+    ['P5', 24],
+    ['P6', 32],
+    ['P7', 245],
+]
+
+
+print(sorted(lista, key=lambda item: item[1]))
